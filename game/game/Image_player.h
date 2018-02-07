43dc313ff720151d6823ae97640176a40838758c
RamkishanPOTHUGANTI <35762247+RamkishanPOTHUGANTI@users.noreply.github.com>
2018-02-07 16:22:59 +0530

    Update Image_player.h

--- game/game/Image_player.h
+++ game/game/Image_player.h
@@ -2,11 +2,12 @@
 #include <SFML\Graphics.hpp>
 class Image_player
 {
+private:
+	int num_strings, * sleep_time;
+	std::string * str;
+
 public:
-	int num_strings,sleep_time;
-	std::string * str; 
-	Image_player(std::string a , int n,int ti);
+	Image_player(std::string a , std::string t , int n);
     int image_display_function(sf::RenderWindow * );
 	~Image_player();
 };
-
